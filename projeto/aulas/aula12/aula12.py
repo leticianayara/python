95222ee3898f4283f2cace00bb488b8ab912a5c0
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 17:42:16 -0300

    Aula 12

---
+++ projeto/aulas/aula12/aula12.py
@@ -0,0 +1,15 @@
+nome = 'Leticia Costa'
+altura = 1.48
+peso = 55
+imc = ...   # peso / (altura * altura)
+
+
+print(nome,'tem',altura,'de altura,')
+print('pesa', peso, 'quilos e seu IMCC é')
+imc = peso / (altura * altura)
+print(imc)
+
+
+# Leticia Costa tem 1.48 de altura,
+# pesa 55 quilos e seu IMC é 
+#  25.109569028487
