20e3ddcc2ac71d09538a1493806712602a1b24c9
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 11:34:43 -0300

    Auka 4

--- projeto/aulas/aula04/aula1.py
+++
@@ -1,4 +0,0 @@
-#Permite escrever um comentario em cima 
-print('Ola') #Permite escrever um comentario na frente
-print(123)
-#Permite escrever um comentario embaixo
---
+++ projeto/aulas/aula04/aula4.py
@@ -0,0 +1,23 @@
+# Tipos int e float
+
+# int -> Números inteiros
+# O tipo int representa qualquer número 
+# positivo ou negativo. int sem sinal é considerado
+# positivo
+print(11)
+print(-11)
+print(0)
+
+# float -> número com ponto flutuante
+# O tipo do float reresenta qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é consitraro positivo
+print(11.0)
+print(-11.0)
+print(0.0)
+
+# A funcão type mostra o tipo que o Python
+# inferiu ao  valor
+print(type(11))
+print(type(11.0),type(1.1),type(0.0) )
+print(type('11.0'))
