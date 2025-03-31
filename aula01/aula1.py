2a0be2f851e8581d0b3c2bd4f39e4c38c3f2b813
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 09:51:17 -0300

    Ajustes

---
+++ aula01/aula1.py
@@ -0,0 +1,19 @@
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdf
+"""
+
+
+'''
+Usar para escrever suas notas
+'''
+
+#Permite escrever um comentario em cima 
+print('Ola') #Permite escrever um comentario na frente
+print(123)
+#Permite escrever um comentario embaixo
+print(456)
