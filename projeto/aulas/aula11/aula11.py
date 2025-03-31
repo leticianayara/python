8edffdc6617a91852b893d8256c8f1ddce407f7f
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 17:14:26 -0300

    Aula 11

---
+++ projeto/aulas/aula11/aula11.py
@@ -0,0 +1,20 @@
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + -
+
+conta_1 = 1 + 1 ** 5 + 5
+print(conta_1)
+print()
+
+conta_2 = (1 + 1) ** 5 + 5
+print(conta_2)
+print()
+
+conta_3 = 1 + 1 ** (5 + 5)
+print(conta_3)
+print()
+
+conta_4 = (1 + 1) ** (5 + 5)
+print(conta_4)
+print()
