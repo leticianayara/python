187913942414d8d0262df3c0ac758af2bf32e604
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 10:21:04 -0300

    Aula 2

--- projeto/aulas/aula02/aula2.py
+++ projeto/aulas/aula02/aula2.py
@@ -1,4 +1,14 @@
-#Permite escrever um comentario em cima 
-print('Ola') #Permite escrever um comentario na frente
-print(123)
-#Permite escrever um comentario embaixo
+# \r\n - CRLF (Windows)
+# \n - LF(MA=ac)
+print(12, 34, 1011, sep='-', end='\r\n')
+print(56, 78, sep="-", end='\n')
+print(9, 10, sep="-", end='\n')
+print()
+print(12, 34, 1011, sep='-', end='##')
+print(56, 78, sep="-", end='\n')
+print()
+print(12, 34, 1011, sep='-', end='##\n')
+print(56, 78, sep="-", end='\n')
+print()
+print(12, 34, 1011, sep='-', end='\n##')
+print(56, 78, sep="-", end='\n')
