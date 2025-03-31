07378c2aead15f2eb9897b234873336d0cbd950a
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 15:09:08 -0300

    Aula 7

---
+++ projeto/aulas/aula07/aula7.py
@@ -0,0 +1,25 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variaveis com letras minusculas, pode usar 
+# números e underline (_).
+# O sinal de = é o operador de atrbuição. Ele é usado para
+# atribuir um  valor a um nome (variavel.
+# Uso: nome_variavel = expressão
+
+nome_completo = 'Leticia Nayara de Lima Costa'
+idade = 38
+soma_dois_mais_dois = 2 + 2
+int_um = bool('1')
+print(int_um, type(int_um))
+print(nome_completo, idade, soma_dois_mais_dois)
+print()
+
+nome = 'Leticia'
+idade = 38
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
+print()
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
