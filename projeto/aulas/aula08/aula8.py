72e087d0b32dce5e52ff06513604071d7c34de4c
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 15:38:29 -0300

    Aula 8

--- projeto/aulas/aula08/aula8.py
+++ projeto/aulas/aula08/aula8.py
@@ -1,4 +1,29 @@
-#Permite escrever um comentario em cima 
-print('Ola') #Permite escrever um comentario na frente
-print(123)
-#Permite escrever um comentario embaixo
+nome = 'Leticia'
+sobrenome = 'Costa'
+idade = 38
+ano_nascimento  = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.48
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
+print()
+
+nome = 'Matheus'
+sobrenome = 'Faria'
+idade = 13
+ano_nascimento  = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.50
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
