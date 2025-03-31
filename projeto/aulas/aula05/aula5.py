042675ad81298f72ba0fe30c74b21a0fd849d2a5
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 12:05:01 -0300

    Aula 5

---
+++ projeto/aulas/aula05/aula5.py
@@ -0,0 +1,11 @@
+# Tipo de dados bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possiveis
+# sim (True) ou não (False).
+# Existem varios operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que
+# questiona se um valor é igual ao outro
+
+print(10 == 10) # True
+print(10 == 11) # False
+print(type(10 == 11))
