a45de3675ddb477412c102f56e1142bc11ba9413
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 10:48:55 -0300

    Aula 3

---
+++ projeto/aulas/aula03/aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de Programação
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+#Strings
+#Aspas Simples
+print('Luiz Otávio')
+print('Luiz "Otávio"')
+
+#Aspas duplas
+print("Luiz Otávio")
+print("Luiz 'Otávio'")
+
+#Escape
+print("Luiz \"Otávio\"")
+
+#r - expressoes regulares
+print(r"Luiz \"Otávio\"")
