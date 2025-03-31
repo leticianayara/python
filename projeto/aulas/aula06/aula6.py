bf529b7953e641d2f57108c1cc91971ba5a4fd6d
Leticia Costa <leticianayaralima@gmail.com>
2025-03-31 14:24:27 -0300

    Auka 6

---
+++ projeto/aulas/aula06/aula6.py
@@ -0,0 +1,22 @@
+# conversão de tipos, coerção 
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+
+#print('1' + 1)
+
+#int()
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+
+#float()
+print(float('1') + 1)
+print(type(float('1') + 1))
+
+#bool()
+print(bool(' '))
+print(bool(''))
+
+#str()
+print(str(11) + 'b')
