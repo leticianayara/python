c9c3a1ad01ad7f8389355a27606d3bd87fcc55ab
Leticia Costa <leticianayaralima@gmail.com>
2025-04-01 10:44:05 -0300

    Aula 18

---
+++ projeto/aulas/aula18/aula18.py
@@ -0,0 +1,22 @@
+#Debbuger do VSCode
+
+#if / elif      /else
+#se / se não se / se não
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Código para a condição 1')
+elif condicao2:
+    ... #outra hora
+elif condicao3:
+    pass #espaço rservado, onde o desenvolvedor vai inserir o código no futuro
+elif condicao4:
+    print('Código para a condição 4')
+else:    
+    print('Nenhuma das condições anteriores')
+
+
+print('Fora do if')
