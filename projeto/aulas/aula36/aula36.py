063a2c8e664ece75d1928bf31fe5e20d9f0e56f2
Leticia Costa <leticianayaralima@gmail.com>
2025-04-02 16:25:04 -0300

    Aula 36 - Operadores de arituicao com aritimeticos

---
+++ projeto/aulas/aula36/aula36.py
@@ -0,0 +1,60 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador += 1
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador += 2
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador -= 2
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador *= 2
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador *= '2'
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador /= 2
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador //= 2
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador **= 2
+print(contador)
+
+print('-' * 15)
+
+contador = 10
+contador %= 2
+print(contador)
