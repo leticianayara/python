91749047876cbce0e7c4b4ab891d0b710039f0ad
Leticia Costa <leticianayaralima@gmail.com>
2025-04-02 16:07:43 -0300

    Aula 35 - while

---
+++ projeto/aulas/aula35/aula35.py
@@ -0,0 +1,20 @@
+"""
+Repetição
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um codigo não tem fim
+"""
+contador = 0
+
+while contador < 10:
+    contador = contador + 1
+    print(contador)
+
+print('-' * 15)
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador = contador + 1    
+
+print('Acabou')
